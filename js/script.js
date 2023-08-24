8ace146cb148cea3480c9b02fc85e7204f1ea666
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2023-08-24 17:11:21 +0200

    Moved js to folder

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 import { showLoader, hideLoader } from "./loader.js";
 
 const API_URL = "https://www.eirikveskje.com/wp-json/wc/v3/products/?consumer_key=ck_b5877d7b70a5804f0ad7e9289848c8613753495b&consumer_secret=cs_b9042d621a1673ff92a770a2094124ab3c2148f0";
